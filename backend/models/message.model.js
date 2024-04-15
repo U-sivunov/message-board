e628ee7bddd2a7756b354b4b1be6fa56847e1366
Urij Sivunov <Sivunov_US@teleformis.ru>
2024-04-16 01:33:58 +0400

    2

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose'
 
 const messageModel = new mongoose.Schema({
     message: {type: String}
-})
+},{ timestamps: true })
 
 export default mongoose.models.message || mongoose.model('message', messageModel);
